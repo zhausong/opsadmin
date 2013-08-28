323174f35307a01487da05c13bcb41c143a09929
itnihao <itnihao@qq.com>
2013-08-28 10:18:41 +0800

    admin.py

---
+++ opsadmin/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from opsadmin.models import Idcmanager,Idcnamelist,Location
+
+admin.site.register(Idcmanager)
+admin.site.register(Idcnamelist)
+admin.site.register(Location)
