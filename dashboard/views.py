3924cb2f2781394a5bb880462402d90baaa42bf7
itnihao <itnihao@qq.com>
2013-08-28 20:56:34 +0800

    Update views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from django.contrib import auth
 from django import forms
-from dashboard.models import User
+#from dashboard.models import User
 import os
 
 def index(req):
@@ -48,30 +48,30 @@ def logout(req):
         return HttpResponseRedirect('/login/')
 
 
-def register(req):
-    context={}
-    if req.method == "POST":
-        username=req.POST.get('username')
-        password=req.POST.get('password')
-        password_two=req.POST.get('password_two')
-        print username,password,password_two
-        user=User.objects.filter(username=username)
-        if user:
-            req.session['username']=username
-            return HttpResponse('用户名已经被占用')
-        elif password == password_two:
-            print "----"
-            user = User()
-            user.username=username
-            user.password=password
-            print "--------"
-            user.save()
-            print username,password,password_two
-            #return HttpResponse(u'恭喜你!注册成功，您的用户名为'+username)
-            return HttpResponseRedirect('/login/',context_instance=RequestContext(req))
-        else:
-            return HttpResponse(u'您两次输入的密码不匹配，请重新输入') 
-    else:
-         uf=UserForm()
-    return render_to_response('register.html',context_instance=RequestContext(req))
+#def register(req):
+#    context={}
+#    if req.method == "POST":
+#        username=req.POST.get('username')
+#        password=req.POST.get('password')
+#        password_two=req.POST.get('password_two')
+#        print username,password,password_two
+#        user=User.objects.filter(username=username)
+#        if user:
+#            req.session['username']=username
+#            return HttpResponse('用户名已经被占用')
+#        elif password == password_two:
+#            print "----"
+#            user = User()
+#            user.username=username
+#            user.password=password
+#            print "--------"
+#            user.save()
+#            print username,password,password_two
+#            #return HttpResponse(u'恭喜你!注册成功，您的用户名为'+username)
+#            return HttpResponseRedirect('/login/',context_instance=RequestContext(req))
+#        else:
+#            return HttpResponse(u'您两次输入的密码不匹配，请重新输入') 
+#    else:
+#         uf=UserForm()
+#    return render_to_response('register.html',context_instance=RequestContext(req))
 
